98a4642e23be1656d7c6c32a17fe8ee89ca04ae2
LukeT1040 <150391839+LukeT1040@users.noreply.github.com>
2023-12-04 13:17:16 -0600

    Add project files.

---
+++ Models/Songs.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment5.Models
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string Artist { get; set; }
+    }
+}
